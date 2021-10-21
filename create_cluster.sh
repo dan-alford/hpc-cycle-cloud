34f27d892c75248e339c098b40f295760619f4ad
dan-alford <31549498+dan-alford@users.noreply.github.com>
2021-10-21 13:25:21 -0500

    Update create_cluster.sh
    Fixed issue, added command to start the cluser

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-cyclecloud initialize --verify-ssl=false --username=$1 --password=$2
+cyclecloud initialize --url=https://localhost:443 --verify-ssl=false --username=$1 --password=$2 --batch
 
 git clone https://github.com/Azure/cyclecloud-slurm.git /tmp/cc-slurm/
 
 cyclecloud import_template -f /tmp/cc-slurm/templates/slurm.txt
 
-cyclecloud create_server Slurm slurm_terra_cluster -p /tmp/parameters.json -P SubnetId=$3 -P Region=$4
+cyclecloud create_cluster Slurm slurm_terra_cluster -p /tmp/parameters.json -P SubnetId=$3 -P Region=$4
+
+cyclecloud start_cluster slurm_terra_cluster
