deaab3db927f64efac70723dd0badfb7a635c435
Cloud User <hpc-cyclecloud-admin@vm-hprc-cyclecloud-on-demand-prod.fn35frtbtowe1e2dox0wf4auua.gx.internal.cloudapp.net>
2021-12-09 21:05:40 +0000

    updating the create cluster and parameters

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-cyclecloud initialize --url=https://localhost:443 --verify-ssl=false --username=$1 --password=$2 --batch
 
-git clone https://github.com/Azure/cyclecloud-slurm.git /tmp/cc-slurm/
 
-cyclecloud import_template -f /tmp/cc-slurm/templates/slurm.txt
 
-cyclecloud create_cluster $3 -p $4 -P SubnetId=$5 -P Region=$6
+cyclecloud create_cluster slurm $1 -p $2  -P AdditionalNFSExportPath="/$3/data" -P AdditonalNFSAddress="$3.blob.core.windows.net"
 
-cyclecloud start_cluster $3
+cyclecloud start_cluster $1
